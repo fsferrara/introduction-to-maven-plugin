664c2f7734433d832e9e7b8115abf2e5517ac278
Saverio Ferrara <fsferrara@users.noreply.github.com>
2025-11-21 17:33:04 +0100

    updates on Fri Nov 21 17:33:04 CET 2025

--- area-of-interest/mobile/ios/ViewsLifecycle/ViewsLifecycle/Views/ComponentsView/Component2.swift
+++ area-of-interest/mobile/ios/ViewsLifecycle/ViewsLifecycle/Views/ComponentsView/Component2.swift
@@ -1,10 +1,36 @@
 import SwiftUI
 
+struct OnViewModifier: ViewModifier {
+    var block: (() -> Void)?
+
+    public init(block action: (() -> Void)? = nil) {
+        self.block = action
+    }
+
+    public func body(content: Content) -> some View {
+        content
+            .onAppear {
+                block?()
+            }
+    }
+}
+
+public extension View {
+    func onViewModifier(block action: (() -> Void)? = nil) -> some View {
+        modifier(OnViewModifier(block: action))
+    }
+}
+
+
 struct Component2: View {
     var body: some View {
         HStack {
-            if false {
-                EmptyView()
+            if true {
+                //EmptyView()
+                Color.clear
+                    .onViewModifier {
+                        LifecycleLogger.log("Component2 - EmptyView", event: "onViewModifier")
+                    }
             }
         }
             .onAppear {
